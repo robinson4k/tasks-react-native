5fc4cfa5c7b20fdda13b65735a6367b49bbfea04
Robinson Silva <robinson.silva@piauiconectado.com.br>
2020-10-13 14:19:12 -0400

    menu ajustes 2

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -9,17 +9,24 @@ import {
 import { DrawerItems } from 'react-navigation-drawer'
 import { Gravatar } from 'react-native-gravatar'
 
+import commonStyles from '../commonStyles'
+
 export default props => {
     return (
         <ScrollView>
             <View style={styles.header}>
+                <Text style={styles.title}>Tasks</Text>
                 <Gravatar style={styles.avatar} options={{
                     email: props.navigation.getParam('email'),
                     secure: true
                 }} />
                 <View style={styles.userInfo}>
-                    <Text>{props.navigation.getParam('email')}</Text>
-                    <Text>{props.navigation.getParam('name')}</Text>
+                    <Text style={styles.name}>
+                        {props.navigation.getParam('name')}
+                    </Text>
+                    <Text style={styles.email}>
+                        {props.navigation.getParam('email')}
+                    </Text>
                 </View>
             </View>
             <DrawerItems {...props} />
@@ -32,13 +39,33 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         borderColor: '#DDD'
     },
+    title: {
+        fontSize: 30,
+        padding: 10,
+        paddingTop: Platform.OS === "ios" ? 50 : 30,
+        fontFamily: commonStyles.fontFamily,
+        color: '#000',
+    },
     avatar: {
         width: 60,
         height: 60,
         borderWidth: 3,
         borderRadius: 30,
         margin: 10,
-        marginTop: Platform.OS === "ios" ? 50 : 10
+        backgroundColor: '#222',
     },
-    userInfo: {}
+    userInfo: {
+        marginLeft: 10
+    },
+    name: {
+        fontSize: 20,
+        marginBottom: 5,
+        fontFamily: commonStyles.fontFamily,
+    },
+    email: {
+        fontSize: 15,
+        marginBottom: 10,
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.subText
+    }
 })
